15e175c8f037e13e24d10232aecba62edb1d7949
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-03-10 18:18:23 -0500

    HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 // import logo from './logo.svg'; 
 import './App.css';
 
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header'; 
+import Footer from './components/Footer';
+
+import HomeScreen from './pages/HomeScreen'
+
 function App() {
   return (
     <div>
-      My app 
+      <Header />
+      {/* py- = padding */}
+      <main className = "py-5"> 
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
