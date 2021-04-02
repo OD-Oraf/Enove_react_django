6adb26bad5d26a5d7230f15c078e97a67d26455f
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-04-01 23:07:11 -0400

    product actions in homepage

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,4 +1,4 @@
-import axios  from 'axois'
+import axios from 'axios'
 
 import { 
     PRODUCT_LIST_REQUEST,
@@ -7,7 +7,7 @@ import {
 } from '../constants/productConstants';
 
 // replace API call in homescreen
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try {
         dispatch({type: PRODUCT_LIST_REQUEST})
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from 'react';
+import { useDispatch,useSelector } from 'react-redux'; 
 
 import { Row,Col } from 'react-bootstrap';
 
 //Component Import
 import Product from '../components/Product';
-import axios from 'axios';
+
+import { listProducts } from '../actions/productActions'
+
+// import axios from 'axios';
 
 //Products.js file import
 // import products from '../products';
@@ -14,30 +18,35 @@ function HomePage() {
     //put products in the state
     //also need django cors header to make this work 
     //when we make a call to the backend api, django has to recognize this call and allow us the retrieve the data 
-    // Concnepts used: use state, async function, promise function, axios, django cors headers
-    const [products, setProducts] = useState([])
+    // Concepts used: use state, async function, promise function, axios, django cors headers
+    // const [products, setProducts] = useState([])
+
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {error, loading, products} = productList 
     useEffect(() => {
-
-        async function fetchProducts(){
-            const { data } = await axios.get('/api/products/')
-            setProducts(data)          
-        }   
-
-        fetchProducts()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
+    
+   
 
 
     return (
         <div>
             <h1> Latest products </h1>
             {/* displaying products in array  */}
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} Lg={4} xL={3} > 
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+            {loading ? <h2>Loading... </h2>
+                : error ? <h3>{error}</h3> 
+                    :
+                    <Row>
+                        {products.map(product => (
+                        <Col key={product._id} sm={12} md={6} Lg={4} xL={3} > 
+                            <Product product={product} />
+                        </Col>
+                        ))}
+                    </Row>   
+            }
+            
         </div>
     )
 }
