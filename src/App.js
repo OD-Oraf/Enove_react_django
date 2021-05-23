415bde518043feb8721a36a0cc81e6aef7c081ea
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-05-23 00:09:52 -0400

    user profile,user details

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path = '/' component = {HomePage} exact/>
           <Route path = '/login' component = {LoginPage} />
           <Route path = '/register' component = {RegisterPage} />
+          <Route path = '/profile' component = {ProfilePage} />
           {/* Render products by id */}
           <Route path = '/product/:id' component = {ProductPage} exact/>
           {/* ? makes the id optional */}
